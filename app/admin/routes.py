f9a8e96e492f6b44b80a42a544a9b6de829ca67d
Mohammed Javaid <43643210+Madd2dogg@users.noreply.github.com>
2019-03-15 21:23:12 -0400

    Comments
    added comments

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,3 +1,5 @@
+#similar to the other routes.py files
+#This will redirect a use to a url on our domain
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_wtf.csrf import CsrfProtect
